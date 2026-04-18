59061081be7966d7cfadb4c9ecf7d7c89ee8cb9b
Deepak <deepaks10591@gmail.com>
2026-04-17 21:41:29 -0400

    RAG implementation added with advisor

--- src/main/java/com/deep_ai/demo/controller/OpenAIController.java
+++ src/main/java/com/deep_ai/demo/controller/OpenAIController.java
@@ -3,6 +3,7 @@ package com.deep_ai.demo.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.model.ChatResponse;
@@ -196,5 +197,11 @@ public class OpenAIController {
 
         System.out.println(text);
         return vectorStore.similaritySearch(text);
+    }
+
+    @PostMapping("/ask")
+    public String getAnswersWithRag(@RequestParam String query){
+
+        return chatClient.prompt(query).advisors(QuestionAnswerAdvisor.builder(vectorStore).build()).call().content();
     }
 }
